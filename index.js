6013322838e27f8f84f1faa3c7b410931ec57338
dmacdo02 <dmacdo02@gmail.com>
2017-01-01 11:30:34 -0500

    Adding test cases and simplifying main function

--- index.js
+++ index.js
@@ -4,23 +4,15 @@ var _ = require('lodash');
 
 // j is the thing to check.
 module.exports = function(j) {
-	if (typeof(j) == 'string') {
+	if (typeof(j) !== 'object') {
 		try {
-			let o = JSON.parse(j);
-
-			// Handle non-exception-throwing cases:
-			// Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-			// but... JSON.parse(null) returns null, and typeof null === "object",
-			// so we must check for that, too. Thankfully, null is falsey, so this suffices:
-			if (o && typeof o === "object" && !_.isEmpty(o)) {
-				return o;
-			} else return false;
+			j = JSON.parse(j);
 		} catch (e) {
 			return false;
 		}
 	}
-	if (typeof(j) == 'object') {
-		if (_.isEmpty(j)) return false;
-		else return j;
-	} else return false;
+
+	//Numbers and booleans that get JSON.parse do not throw an error and are still of type number and boolean.
+	//When null gets JSON.parse it becomes an empty object. So the empty check should suffice.
+	return (typeof(j) == 'object' && !_.isEmpty(j)) ? j : false;
 }
--- index.spec.js
+++ index.spec.js
@@ -14,27 +14,31 @@ lme.w("TESTS");
 lme.wline();
 
 describe("testing isGoodJSON", function() {
-  forEach([
-    ['should return false if , is missing', '{ha: "hi",	meuo: "ho" cat: "fluff"}', false],
-    ['should return false if { mismatch is given.', '{"me": "sas", ["as":5}]', false],
-    ['should return false if , missing (type 2) is given.', '[{"ths":asdf}{"adasd":asdf}]', false],
-    ['should return false if excess , is given.','[{"fasd":"adsf"},]', false],
-    ['should return the parsed object if a good json (type 1) is given.', '{"this": "is","a": "good json"}', 'object'],
-    ['should return the parsed object if a good array (type 1) is given.', '[{"this": "is","a": "good json"}]', 'array'],
-    ['should return false if empty array is given.', '[]', false],
-    ['should return false if empty object is given.', '{}', false],
-    ['should return false if a number is given.', '123', false],
-    ['should return false if a object is given. (NOT STRING)', {}, false],
-    ['should return false if a array is given. (NOT STRING)', [], false],
-    ['should return false if a number is given. (NOT STRING)', 123, false]
-  ])
+	forEach([
+		['should return false if , is missing', '{ha: "hi",	meuo: "ho" cat: "fluff"}', false],
+		['should return false if { mismatch is given.', '{"me": "sas", ["as":5}]', false],
+		['should return false if , missing (type 2) is given.', '[{"ths":asdf}{"adasd":asdf}]', false],
+		['should return false if excess , is given.','[{"fasd":"adsf"},]', false],
+		['should return the parsed object if a good json (type 1) is given.', '{"this": "is","a": "good json"}', 'object'],
+		['should return the parsed object if a good array (type 1) is given.', '[{"this": "is","a": "good json"}]', 'array'],
+		['should return false if empty array is given.', '[]', false],
+		['should return false if empty object is given.', '{}', false],
+		['should return false if a number is given.', '123', false],
+		['should return false if null is given.', 'null', false],
+		['should return false if boolean is given.', 'true', false],
+		['should return false if a object is given. (NOT STRING)', {}, false],
+		['should return false if a array is given. (NOT STRING)', [], false],
+		['should return false if a number is given. (NOT STRING)', 123, false],
+		['should return false if null is given. (NOT STRING)', null, false],
+		['should return false if boolean is given. (NOT STRING)', true, false],
+	])
 	.it("%s", (testCaseName, input, expectedOutput) => {
-    var result = isGoodJSON(input);
+		var result = isGoodJSON(input);
 
-    if (typeof expectedOutput === 'boolean') {
-      expect(result).to.equal(expectedOutput);
-    } else {
-      expect(result).to.be.an(expectedOutput);
-    }
+		if (typeof expectedOutput === 'boolean') {
+			expect(result).to.equal(expectedOutput);
+		} else {
+			expect(result).to.be.an(expectedOutput);
+		}
 	});
 });
