2587abc1f41fd1ef22e93e5b3442da644d896cb7
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-06-08 15:16:58 +0530

    linting stuffs

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var _ = require('lodash');
+const _ = require('lodash');
 
-// j is the thing to check.
-module.exports = function(j) {
-	if (typeof(j) !== 'object') {
+// J is the thing to check.
+module.exports = function (j) {
+	if (typeof (j) !== 'object') {
 		try {
 			j = JSON.parse(j);
-		} catch (e) {
+		} catch (err) {
 			return false;
 		}
 	}
 
-	//Numbers and booleans that get JSON.parse do not throw an error and are still of type number and boolean.
-	//When null gets JSON.parse it becomes an empty object. So the empty check should suffice.
-	return (typeof(j) == 'object' && !_.isEmpty(j)) ? j : false;
-}
+	// Numbers and booleans that get JSON.parse do not throw an error and are still of type number and boolean.
+	// When null gets JSON.parse it becomes an empty object. So the empty check should suffice.
+	return (typeof (j) === 'object' && !_.isEmpty(j)) ? j : false;
+};
--- index.spec.js
+++ index.spec.js
@@ -1,19 +1,26 @@
+/* global describe */
 'use strict';
 
-// test
+// Test
 // ====
-var expect = require('chai').expect;
-var lme = require('lme');
-var _ = require('lodash');
-var forEach = require('mocha-each');
+const expect = require('chai').expect;
+const lme = require('lme');
+const forEach = require('mocha-each');
 
-// pull our heroine
-var isGoodJSON = require('./index');
+// Pull our heroine
+let isGoodJSON;
 
-lme.w("TESTS");
+try {
+	isGoodJSON = require('./index');
+} catch (err) {
+	lme.e('index file not found', err);
+	throw new Error(err);
+}
+
+lme.w('TESTS');
 lme.wline();
 
-describe("testing isGoodJSON", function() {
+describe('testing isGoodJSON', () => {
 	forEach([
 			['should return false if , is missing', '{ha: "hi",	meuo: "ho" cat: "fluff"}', false],
 			['should return false if { mismatch is given.', '{"me": "sas", ["as":5}]', false],
@@ -30,10 +37,10 @@ describe("testing isGoodJSON", function() {
 			['should return false if a array is given. (NOT STRING)', [], false],
 			['should return false if a number is given. (NOT STRING)', 123, false],
 			['should return false if null is given. (NOT STRING)', null, false],
-			['should return false if boolean is given. (NOT STRING)', true, false],
-		])
-		.it("%s", function(testCaseName, input, expectedOutput) {
-			var result = isGoodJSON(input);
+			['should return false if boolean is given. (NOT STRING)', true, false]
+	])
+		.it('%s', (testCaseName, input, expectedOutput) => {
+			const result = isGoodJSON(input);
 
 			if (typeof expectedOutput === 'boolean') {
 				expect(result).to.equal(expectedOutput);
